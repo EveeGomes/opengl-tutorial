7a8f72e8526523d7346871365b5eca7ce49af94f
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-06-01 11:56:48 -0300

    Set opengl attributes and setup opengl context.

--- Main.cpp
+++ Main.cpp
@@ -17,6 +17,20 @@ void InitializeProgram()
       exit(1);
    }
 
+   // After initializing SDL and before creating the window, set some opengl properties/attributes
+   
+   // Set version 4.1 (working on Mac, Linux, Windows)
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+   // Use PROFILE_CORE because it disables deprecated functions which guarantees old and unsupported functions won't get used
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+   // Double buffering allows for smoother transition of things
+   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+   // Depth buffer gets 24 bits so we get more precision when determining objects or overlapping 
+   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
    // Create a window
   gGraphicApplicationWindow = SDL_CreateWindow("OpenGL Window",
                                                 0, 0, 
@@ -27,6 +41,14 @@ void InitializeProgram()
   {
      std::cout << "SDL_Window was not able to be created"
                << std::endl;
+     exit(1);
+  }
+
+  // Setup opengl context
+  gOpenGLContext = SDL_GL_CreateContext(gGraphicApplicationWindow);
+  if (gOpenGLContext)
+  {
+     std::cout << "OpenGL context not available\n";
      exit(1);
   }
 }
