106ae5cf2d7af08089443faadb8d09b8cf0ce851
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-17 19:09:42 -0300

    Include glm

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,14 @@
 #include <string>
 #include <fstream>
 
+// Include all GLM core / GLSL features
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+
+// Include all GLM extensions
+#include <glm/ext.hpp> // perspective, translate, rotate
+
+
+
 // VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV Globals VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 
 /** Setting up SDL */
